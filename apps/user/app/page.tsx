b4435d866f29981a6b9c229a2799a630266bedad
Deependra Parmar <104254575+DeependraParmar@users.noreply.github.com>
2024-05-05 06:21:02 +0530

    Recoil set up and used

--- apps/user/app/page.tsx
+++ apps/user/app/page.tsx
@@ -1,8 +1,17 @@
+"use client"
+
 import React from 'react'
+import { PrismaClient } from '@repo/db/client';
+import { useBalance } from "@repo/store/useBalance";
+const client = new PrismaClient();
 
 const page = () => {
+  const balance = useBalance();
   return (
-    <div className='text-2xl'>Hi there</div>
+    <>
+      <div className='text-2xl'>Hi there</div>
+      <div>Your balance is: {balance}</div>
+    </>
   )
 }
 
